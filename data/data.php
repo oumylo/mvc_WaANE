f44d3e63742bc31f3e883e64aa3c6192131cdfb4
oumylo <oumylo611@hmail.com>
2026-07-02 02:08:51 +0000

    feat: quelques rectification sur data

--- data/data.php
+++ data/data.php
@@ -13,38 +13,25 @@ $clients = [
 ];
 
 $commandes = [
-    0=>[
-        'client'=>1,
-        'date'=>'12/06/2026',
-        'montant'=>0,
-        'etat'=>'PAYER',
-        'product'=>[
-            0=>['quantite'=>5,'productIndex'=>0],
-            1=>['quantite'=>2,'productIndex'=>1],
-        ],
-        'paiement'=>'reference1'
+    0=>['client'=>1,'date'=>'12/06/2026','montant'=>0,'etat'=>'PAYER',
+    'product'=>[
+        0=>['quantite'=>5,'productIndex'=>0],
+        1=>['quantite'=>2,'productIndex'=>1],
+    ],
+    'paiement'=>'reference1'
     ],
 
-    1=>[
-        'client'=>1,
-        'date'=>'16/06/2026',
-        'montant'=>0,
-        'etat'=>'IMPAYER',
-        'product'=>[
-            0=>['quantite'=>7,'productIndex'=>0],
-            1=>['quantite'=>3,'productIndex'=>1],
-        ],
-        'paiement'=>null
+    1=>['client'=>1,'date'=>'16/06/2026','montant'=>0,'etat'=>'IMPAYER',
+    'product'=>[
+        0=>['quantite'=>7,'productIndex'=>0],
+        1=>['quantite'=>3,'productIndex'=>1],
+    ],
+    'paiement'=>null
     ]
 ];
 
 $paiements = [
-    0=>[
-        'date'=>'20/06/2026',
-        'reference'=>'reference1',
-        'facture'=>[
-            'date'=>'14/06/2026',
-            'reference'=>1
-        ]
+    0=>['date'=>'20/06/2026','reference'=>'reference1',
+    'facture'=>['date'=>'14/06/2026','reference'=>1],
     ]
 ];
