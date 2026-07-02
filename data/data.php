20492f5c959c6615a0c8a632ba62a6ecd4526792
oumylo <oumylo611@hmail.com>
2026-07-02 01:56:23 +0000

    feat: initialisation de la structure MVC

---
+++ data/data.php
@@ -0,0 +1,50 @@
+<?php
+
+$products = [
+    0=>['ref'=>'ref1','libele'=>'lib1','prix'=>2000,'quantite'=>12],
+    1=>['ref'=>'ref2','libele'=>'lib2','prix'=>500,'quantite'=>2],
+];
+
+$productsArchived = [];
+
+$clients = [
+    0=>['nomPrenom'=>'Adama Timera','tel'=>'770001233','address'=>'yeumbeul'],
+    1=>['nomPrenom'=>'mami Cisse','tel'=>'771001233','address'=>'grand Dakar'],
+];
+
+$commandes = [
+    0=>[
+        'client'=>1,
+        'date'=>'12/06/2026',
+        'montant'=>0,
+        'etat'=>'PAYER',
+        'product'=>[
+            0=>['quantite'=>5,'productIndex'=>0],
+            1=>['quantite'=>2,'productIndex'=>1],
+        ],
+        'paiement'=>'reference1'
+    ],
+
+    1=>[
+        'client'=>1,
+        'date'=>'16/06/2026',
+        'montant'=>0,
+        'etat'=>'IMPAYER',
+        'product'=>[
+            0=>['quantite'=>7,'productIndex'=>0],
+            1=>['quantite'=>3,'productIndex'=>1],
+        ],
+        'paiement'=>null
+    ]
+];
+
+$paiements = [
+    0=>[
+        'date'=>'20/06/2026',
+        'reference'=>'reference1',
+        'facture'=>[
+            'date'=>'14/06/2026',
+            'reference'=>1
+        ]
+    ]
+];
